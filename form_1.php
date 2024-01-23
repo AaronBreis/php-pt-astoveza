f519c89f6367605d367894bb6818d596bab13a53
unknown <aaronastovezabreis@gmail.com>
2024-01-22 19:15:05 -0800

    New File

---
+++ form_1.php
@@ -0,0 +1,11 @@
+<html> 
+<body>
+
+Welcome <?php echo $_POST["firstname"] , $_["lastname"]; ?><br>
+
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your addresds is: <?php echo $_POST["address"]; ?>
+
+</body>
+</html>
+
